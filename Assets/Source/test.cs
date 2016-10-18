901d7a1d5f340440222e76ad1b3464344877e1a6
Benjamin <benj.bach@googlemail.com>
2016-10-17 21:00:40 -0400

    create test

---
+++ Assets/Source/test.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	public DataElement{
+		string name;
+		string gender;
+		int age;
+		int weight;
+
+		public DataElement(string name, string gender, int age, int weight){
+			this.name = name; 
+			this.gender = gender;
+			this.age = age;
+			this.weight = weight;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		List<DataElement> data = initData();		
+
+		// Create rectangles
+		Selection s = d4.selectAll()
+			.data(data)
+			.append("sphere")
+				.attr("x", (d,i)=>{ return d.age; })
+				.attr("y", (d,i)=>{ return d.weight; })
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	List<DataElement> initData(){
+		List<DataElement> data = new List<DataElement>();
+		data.Add(new DataElement("Arno", "m", 25, 73));
+		data.Add(new DataElement("Bea", "f", 23, 53));
+		data.Add(new DataElement("Christophe", "m", 37, 71));
+		data.Add(new DataElement("Denver", "m", 53, 97));
+		data.Add(new DataElement("Eva", "f", 53, 98));		
+		return data;
+	}
+}
