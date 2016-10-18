6d7a502c4e30405669e389368d1bbb340fa1a4cd
Benjamin <benj.bach@googlemail.com>
2016-10-18 12:04:43 -0400

    edit Selecton.cs

--- Assets/Source/Selection.cs
+++ Assets/Source/Selection.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEngine;
+using Test;
 
 public class Selection {
 
     float DEFAULT_PRIMITIVE_SIZE = 0.01f;
     float DEFAULT_LINE_THICKNESS = 0.001f;
 
-    List<Object> dataElements = new List<Object>();
-    List<Object> filteredDataElements = new List<Object>();
+	List<test.DataObject> dataElements = new List<test.DataObject>();
+	List<test.DataObject> filteredDataElements = new List<test.DataObject>();
     List<UnityEngine.GameObject> visualElements = new List<UnityEngine.GameObject>();
     Delegate overHandler ;
     Delegate moveHandler;
@@ -30,14 +32,14 @@ public class Selection {
     //}
 
     /// Binds data obejcts to indices
-    Selection data(List<Object> objects)
+	public Selection data(List<test.DataObject> objects)
     {
-        dataElements = new List<Object>(objects);
-        filteredDataElements = new List<Object>(objects);
+		dataElements = new List<test.DataObject>(objects);
+		filteredDataElements = new List<test.DataObject>(objects);
         return this;
     }
 
-    Selection append(string _shape)
+	public Selection append(string _shape)
     {
 
         switch(_shape){
@@ -59,58 +61,77 @@ public class Selection {
         return this;
     }
 
-    private void createCylinder()
+  
+
+	public Selection attr(string attributeName, System.Object val )
     {
-        for (int i = 0; i < filteredDataElements.Count; i++)
+		for(int i = 0 ; i < this.dataElements.Count; i++ )
         {
-            //creates a default sphere
-            UnityEngine.GameObject go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cylinder);
-            //scales it down to 1cm
-            go.transform.localScale *= DEFAULT_PRIMITIVE_SIZE;
-            UnityEngine.Vector3 _scale = go.transform.localScale;
-            _scale.y = DEFAULT_PRIMITIVE_SIZE*2f;
-            go.transform.localScale = _scale;
-            //add the created GameObject to the visual elements
-            visualElements.Add(go);
+			setAttr (attributeName, val, this.visualElements [i]);
         }
+		return this;
     }
-
-
-    public Selection attr(string:attributeName, Object val){
-        for(int i=0 ; i<this.dataElements.length  i++){
-            setAttr(attributeName, val, this.visualElements[i])
+	public Selection attr(string attributeName, Func<System.Object,int,System.Object> func)
+    {
+		for(int i=0 ; i<this.dataElements.Count ; i++){
+			System.Object val = func (this.dataElements [i], i);
+			setAttr(attributeName, val, this.visualElements[i]);
         }
+		return this;
     }
-    private setAttr(string:attributeName, Object val, GameObject visualObject)
+	private void setAttr(string attributeName, System.Object val, GameObject visualObject)
+    {
         switch(attributeName){
-            case "x": setPosX(visualObject, val)
-            case "y": setPosY(visualObject, val)
-            case "z": setPosZ(visualObject, val)                            
+		case "x": setPosX (visualObject, (float) val); break;
+		case "y": setPosY(visualObject, (float) val); break;
+		case "z": setPosZ(visualObject, (float) val); break;                           
         }
+
     }
-    private setPosX(visualObject, val){
+	private void setPosX(GameObject visualObject, float val)
+    {
         var pos = visualObject.transform.position;
         pos.x = val;
         visualObject.transform.position = pos;
+
     }
-    private setPosY(visualObject, val){
+	private void setPosY(GameObject visualObject, float val)
+    {
         var pos = visualObject.transform.position;
         pos.y = val;
         visualObject.transform.position = pos;
     }
-    private setPosZ(visualObject, val){
+	private void setPosZ(GameObject visualObject, float val)
+    {
         var pos = visualObject.transform.position;
         pos.z = val;
         visualObject.transform.position = pos;
     }
     
 
-    
-    
+
+   
 
     /////////////////////////
     /// CREATOR FUNCTIONS /// 
     /////////////////////////
+
+
+	private void createCylinder()
+	{
+		for (int i = 0; i < filteredDataElements.Count; i++)
+		{
+			//creates a default sphere
+			UnityEngine.GameObject go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cylinder);
+			//scales it down to 1cm
+			go.transform.localScale *= DEFAULT_PRIMITIVE_SIZE;
+			UnityEngine.Vector3 _scale = go.transform.localScale;
+			_scale.y = DEFAULT_PRIMITIVE_SIZE*2f;
+			go.transform.localScale = _scale;
+			//add the created GameObject to the visual elements
+			visualElements.Add(go);
+		}
+	}
 
 
     private void createPolygons()
@@ -163,16 +184,16 @@ public class Selection {
 
     private void createLines()
     {
-        for (int i = 0; i < filteredDataElements.Count; i++)
-        {
-            //creates a default sphere
-            UnityEngine.GameObject go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.LineRenderer);
-            //scales it down to 1cm
-            go.SetWidth(DEFAULT_LINE_THICKNESS, DEFAULT_LINE_THICKNESS);
-            // go.transform.localScale *= DEFAULT_PRIMITIVE_SIZE;
-            //add the created GameObject to the visual elements
-            visualElements.Add(go);
-        }
+//        for (int i = 0; i < filteredDataElements.Count; i++)
+//        {
+//            //creates a default sphere
+//            UnityEngine.GameObject go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.LineRenderer);
+//            //scales it down to 1cm
+//            go.SetWidth(DEFAULT_LINE_THICKNESS, DEFAULT_LINE_THICKNESS);
+//            // go.transform.localScale *= DEFAULT_PRIMITIVE_SIZE;
+//            //add the created GameObject to the visual elements
+//            visualElements.Add(go);
+//        }
     }
 
     private void createPaths()
--- Assets/Source/test.cs
+++ Assets/Source/test.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 
+namespace Test{
 public class test : MonoBehaviour {
 
-	public class DataElement{
-		string name;
-		string gender;
-		int age;
-		int weight;
+	public class DataObject{
+		public static string name;
+		public static string gender;
+		public static int age;
+		public static int weight;
 
-		public DataElement(string name, string gender, int age, int weight){
+		public DataObject(string name, string gender, int age, int weight){
 			this.name = name; 
 			this.gender = gender;
 			this.age = age;
@@ -21,16 +22,15 @@ public class test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		List<DataElement> data = initData();		
+		List<DataObject> data = initData();		
 
 		// Create rectangles
-		Selection s = d4.selectAll()
-			.data(data)
-			.append("sphere")
-				.attr("x", (d,i)=>{ return d.age; })
-				.attr("y", (d,i)=>{ return d.weight; })
-				// .style("fill", "#ff0000")
-
+		Selection s = d4.selectAll ()
+			.data (data)
+			.append ("sphere")
+				.attr ("x", (d, i) => { return (DataObject)d.age; })
+				.attr ("y", (d, i) => { return (DataObject)d.weight; });
+				// .style("fill", "#
 	}
 	
 	// Update is called once per frame
@@ -39,13 +39,14 @@ public class test : MonoBehaviour {
 	}
 
 
-	List<DataElement> initData(){
-		List<DataElement> data = new List<DataElement>();
-		data.Add(new DataElement("Arno", "m", 25, 73));
-		data.Add(new DataElement("Bea", "f", 23, 53));
-		data.Add(new DataElement("Christophe", "m", 37, 71));
-		data.Add(new DataElement("Denver", "m", 53, 97));
-		data.Add(new DataElement("Eva", "f", 53, 98));		
+	List<DataObject> initData(){
+		List<DataObject> data = new List<DataObject>();
+		data.Add(new DataObject("Arno", "m", 25, 73));
+		data.Add(new DataObject("Bea", "f", 23, 53));
+		data.Add(new DataObject("Christophe", "m", 37, 71));
+		data.Add(new DataObject("Denver", "m", 53, 97));
+		data.Add(new DataObject("Eva", "f", 53, 98));		
 		return data;
 	}
+}
 }
